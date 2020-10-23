9ce649eef71264782405ba86fabb2ae2176217f5
JedrzejBronislaw <jedkru@wp.pl>
2020-10-23 22:05:34 +0200

    Create first window

--- src/jedrzejbronislaw/propcalc/main/Main.java
+++ src/jedrzejbronislaw/propcalc/main/Main.java
@@ -1,9 +1,46 @@
 package jedrzejbronislaw.propcalc.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private static final String WINDOW_TITLE = "PropCalc";
+	
+	private Stage stage;
+	
 
 	public static void main(String[] args) {
 		System.out.println("PropCalc");
+		Main.launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		stage = primaryStage;
+		buildView();
+	}
+	
+	private void buildView() {
+		stage.setScene(buildScene());
+		stage.setWidth(1000);
+		stage.setHeight(700);
+		stage.setTitle(WINDOW_TITLE);
+		stage.setOnCloseRequest(e -> {
+			Platform.exit();
+			System.exit(0);
+		});
+		
+		stage.show();
+	}
+
+	private Scene buildScene() {
+		BorderPane pane = new BorderPane(new Label(WINDOW_TITLE));
+		
+		return new Scene(pane);
+	}
 }
