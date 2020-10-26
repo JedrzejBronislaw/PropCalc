8fa811ba0c5152c543f2c83e35789e18c0a8f558
JedrzejBronislaw <jedkru@wp.pl>
2020-10-26 21:29:28 +0100

    Add dynamic root FXML in MyFXMLLoader class

--- src/jedrzejbronislaw/propcalc/tools/MyFXMLLoader.java
+++ src/jedrzejbronislaw/propcalc/tools/MyFXMLLoader.java
@@ -23,9 +23,13 @@ public class MyFXMLLoader<T extends Initializable> {
 		@NonNull @Getter private T controller;
 	}
 	
+	
+	private static String path(String resource) {
+		return MAIN_DIR + "/"  + resource;
+	}
 
 	private URL url(String resource) {
-		return getClass().getResource(MAIN_DIR + "/"  + resource);
+		return getClass().getResource(path(resource));
 	}
 	
 	public NodeAndController<T> create(String fxmlFilePath) {
@@ -43,5 +47,24 @@ public class MyFXMLLoader<T extends Initializable> {
 		}
     	
 		return new NodeAndController<T>(node, fxmlLoader.getController());
+	}
+	
+	public static boolean create(String fxmlFilePath, Node node) {
+		FXMLLoader fxmlLoader = new FXMLLoader();
+
+		fxmlLoader.setLocation(node.getClass().getResource(path(fxmlFilePath)));
+		fxmlLoader.setResources(ResourceBundle.getBundle(lang));
+		
+		fxmlLoader.setRoot(node);
+		fxmlLoader.setController(node);
+		
+		try {
+			fxmlLoader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
 	}
 }
