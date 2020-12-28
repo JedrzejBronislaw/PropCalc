e2a4f53c2d634f9823609d7be1f5fcbc17ee8c34
JedrzejBronislaw <jedkru@wp.pl>
2020-12-28 21:08:38 +0100

    Add tests for Solution class (property: substance)

---
+++ test/jedrzejbronislaw/propcalc/model/SolutionTest_Substance5.java
@@ -0,0 +1,45 @@
+package jedrzejbronislaw.propcalc.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import jedrzejbronislaw.propcalc.substances.Substance;
+
+public class SolutionTest_Substance5 {
+
+	private static Solution solution;
+	
+	@BeforeClass
+	public static void prepare() {
+		Substance substance = new Substance("", "", "", 5f);
+		
+		solution = new Solution();
+		solution.setSubstance(substance);
+		solution.setVolume(1); //1 ml
+		solution.setConcentration(1); //1 mg/ml
+		solution.setProportion(1);
+	}
+	
+	@Test
+	public void testMassOfSubstance() {
+		assertEquals(1, solution.massOfSubstance(), 0); //1 mg
+	}
+
+	@Test
+	public void testNumberOfMolecules() {
+		double expected = 1.204428152e+20;
+		double actual = solution.numberOfMolecules();
+		
+		double relativeError = (Math.abs(expected-actual)/expected);
+		
+		assertTrue(relativeError < 0.00001);
+	}
+
+	@Test
+	public void testProportionOfVolume() {
+		assertEquals(5f, solution.proportionOfVolume(), 0);
+	}
+}
